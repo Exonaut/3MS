a524000a293b8bac0ef4a46bbb9f8128023b5544
Exonaut <sven.reinemuth@web.de>
2023-02-02 08:47:41 +0100

    win/loose sounds

--- Assets/GUI/EndGameScreen/EndGameUIManager.cs
+++ Assets/GUI/EndGameScreen/EndGameUIManager.cs
@@ -3,22 +3,30 @@ using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class EndGameUIManager : MonoBehaviour
 {
     [FoldoutGroup("Dependencies")][SerializeField, Required] private MenuUI winUI;
     [FoldoutGroup("Dependencies")][SerializeField, Required] private MenuUI looseUI;
 
+    [FoldoutGroup("Sounds")][SerializeField, Required] private AudioClip looseSound;
+    [FoldoutGroup("Sounds")][SerializeField, Required] private AudioClip winSound;
+
     private void Update()
     {
         Cursor.lockState = CursorLockMode.None;
 
+        var audioSource = GetComponent<AudioSource>();
+
         if (GameGlobals.gameWon)
         {
             winUI.ShowUI(this);
+            audioSource.PlayOneShot(winSound);
             return;
         }
 
         looseUI.ShowUI(this);
+        audioSource.PlayOneShot(looseSound);
 
         enabled = false;
     }
